4a5c4c38e80a67b2168e1f9896cbcd28652748ff
= <=>
2023-07-31 13:21:36 +0500

    fib recursive func

--- main.py
+++ main.py
@@ -4,5 +4,15 @@ def add(a, b):
     return a + b
 
 
-def fib():
-    ...
+def fib(n):
+    f1, f2 = 1, 1
+
+    for i in range(n):
+        yield f1
+        f1, f2 = f2, f1 + f2
+
+
+def fib_recurs(n):
+    if n in (1, 2):
+        return 1
+    return fib(n - 1) + fib(n - 2)
