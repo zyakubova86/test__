f72a9eefe7f93a381eac99762ca8d2a618ae5f99
= <=>
2023-07-31 15:10:07 +0500

    add test.py

--- main.py
+++ main.py
@@ -4,22 +4,6 @@ def add(a, b):
     return a + b
 
 
-def fib(n):
-    f1, f2 = 1, 1
-
-    for i in range(n):
-        yield f1
-        f1, f2 = f2, f1 + f2
 
 
-def fib_recurs(n):
-    if n in (1, 2):
-        return 1
-    return fib(n - 1) + fib(n - 2)
-
-# Another change test
-print("test2")
-
-# changes from repo 
-print("Testing...")
 
