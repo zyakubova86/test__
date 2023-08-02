64442093d3d46a6cce5ec3495e7eae348b26100c
= <=>
2023-08-02 10:11:30 +0500

    zadacha uroka

--- test.py
+++ test.py
@@ -1,2 +1,34 @@
-print("Hello from master")
+import numpy as np
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# 1
+# for i in a:
+#     if i % 2 == 0:
+#         print(i)
+
+# 2
+# i = 1
+# while i <= len(a):
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+
+# 3
+# l = np.array(a)
+# l2 = l[l % 2 == 0]
+# print(l2)
+
+
+# 4
+# a2 = list(filter(lambda a: (a % 2 == 0), a))
+# print(a2)
+
+
+# 5 recurs
+
+
+
+# [2, 4, 6, 8, 10]
 
